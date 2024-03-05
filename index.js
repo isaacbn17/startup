58fac18154945921bbcf4ad1b2270d8aff7776ac
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-05 16:38:41 -0700

    index.js material

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express)');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/results', (_req, res) => {
+    res.send(results);
+});
+
+apiRouter.post('/result', (req, res) => {
+    results = displayResults(req.body, results);
+    res.send(results);
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public'});
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

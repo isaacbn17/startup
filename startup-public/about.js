1f7108882ef4e3234695e45a196b58a493bcff11
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-08 08:36:37 -0700

    Gets a random picture

--- startup-public/about.js
+++ startup-public/about.js
@@ -1,3 +1,20 @@
+function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+  
+        const width = containerEl.offsetWidth;
+        const height = containerEl.offsetHeight;
+  
+        const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', imgUrl);
+        containerEl.appendChild(imgEl);
+      });
+  }
+
 function displayQuote(data) {
     fetch('https://api.quotable.io/random')
       .then((response) => response.json())
@@ -17,10 +34,5 @@ function displayQuote(data) {
       });
   }
 
+displayPicture();
 displayQuote();
-
-  // let chat = 'Happy birthday to you';
-// setInterval(() => {
-//     chat = `${chat} and you`;
-//     document.querySelector('#quote_box').textContent = chat;
-// }, 7000);

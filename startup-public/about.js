99f8f063e2bf89aaab6380dab75a880078202003
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-08 08:32:45 -0700

    Gets a random quote

--- startup-public/about.js
+++ startup-public/about.js
@@ -1,5 +1,26 @@
-let chat = 'Happy birthday to you';
-setInterval(() => {
-    chat = `${chat} and you`;
-    document.querySelector('#quote_box').textContent = chat;
-}, 7000);
+function displayQuote(data) {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote_box');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('quote');
+  
+        quoteEl.textContent = data.content;
+        authorEl.textContent = data.author;
+  
+        containerEl.appendChild(quoteEl);
+        containerEl.appendChild(authorEl);
+      });
+  }
+
+displayQuote();
+
+  // let chat = 'Happy birthday to you';
+// setInterval(() => {
+//     chat = `${chat} and you`;
+//     document.querySelector('#quote_box').textContent = chat;
+// }, 7000);

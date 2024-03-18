191419143ab10d41f985da997fe9fa8bee22368a
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-18 13:17:18 -0600

    Database javascript file

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const config = require('./dbConfig.json');
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+// const { MongoClient } = require('mongodb')
+// const config = require('./dbConfig.json');
+
+// const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+// const client = new MongoClient(url);
+
+// (async function testConnection() {
+//     await client.connect();
+//     await db.command({ ping: 1 });
+//   })().catch((ex) => {
+//     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+//     process.exit(1);
+//   });
 
 const express = require('express');
 const app = express();

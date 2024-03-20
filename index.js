716c93765abc9908eb469cbf5adeddfe4e496e90
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-20 07:54:34 -0600

    Index.js connects to database.js and gets info about users

--- index.js
+++ index.js
@@ -33,7 +33,47 @@ apiRouter.post('/auth/create', async (req, res) => {
     }
 });
 
+// Gets authentication token for the user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            setAuthCookie(res, user.token);
+            res.send({ id: user._id });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
 
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+// Returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+    const user = await DB.getUser(req.params.email);
+    if (user) {
+        const token = req?.cookies.token;
+        res.send({ email: user.email, authenticated: token === user.token });
+        return;
+    }
+    res.status(404).send({ msg: 'Unkown' });
+});
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+    authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+});
 
 // Creates a new survey
 apiRouter.post('/survey', (req, res) => {

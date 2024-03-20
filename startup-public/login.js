a8ef5ac7be8ab3cc2e2306964de88e3b9658eb7e
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-03-20 08:18:10 -0600

    Login process

--- startup-public/login.js
+++ startup-public/login.js
@@ -1,5 +1,13 @@
-function login () {
-    const emailEl = document.querySelector('#inputemail');
-    localStorage.setItem('userName', emailEl.value);
-    window.location.href = 'survey.html';
+async function login () {
+    const emailEl = document.querySelector('#inputemail').value;
+    const password = document.querySelector('#userPassword').value;
+    const response = await fetch(`api/auth/login`, {
+        method: 'post',
+        body: JSON.stringify({ email: emailEl, password: password }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    // localStorage.setItem('userName', emailEl.value);
+    // window.location.href = 'survey.html';
 }

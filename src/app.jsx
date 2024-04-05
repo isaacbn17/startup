4a591e4e11ddb52c0f6fcdbba38c000c794d4c4e
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-04-05 09:32:53 -0600

    Index.jsx, app.css, app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return <div className="bg-success" style="--bs-bg-opacity: .5;">App will display here</div>
+}

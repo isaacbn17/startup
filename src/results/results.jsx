43b2ea80e915731d56202f407d986f7dd8924252
Isaac Neuenschwander <ibneuenschwander@gmail.com>
2024-04-10 08:20:55 -0600

    Html for results page

--- src/results/results.jsx
+++ src/results/results.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export function Results() {
     return (
-        <main>
-            <div>Results displayed here</div>
+        <main className="results">
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Question</th>
+                        <th>Answer 1</th>
+                        <th>Answer 2</th>
+                        <th>Answer 3</th>
+                        <th>Answer 4</th>
+                    </tr>
+                </thead>
+                <tbody id="resultsContainer"></tbody>
+            </table>
+
+            <div id="surveyNotifications"></div>        
         </main>
     )
 }
